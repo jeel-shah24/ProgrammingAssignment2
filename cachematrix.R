3313c4394b33e29b83e4138e49c08ba9af837723
jeel-shah24 <jeelshah.2412@gmail.com>
2017-07-22 23:29:42 +0530

    Updated makeCachematrix()

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,25 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The compution involved in matrix inverse is often costly
+## The following program improves upon this cost by creating a cache of the matrix inverse 
+## which can be checked before any further computation
 
-## Write a short comment describing this function
+## The makeCacheMatrix is a basic function that is responsible for creating a matrix
+## This can set the matrix, get the matrix and it can also set or get the inverse of this matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+  inverse<- NULL
+  set <- function(new){
+   x <<- new
+   inverse<<- NULL
+  }
+  get <- function(){
+    x
+  }
+  set_inverse <- function(new_inverse){
+    inverse <<- new_inverse
+  }
+  get_inverse <- function(){
+    inverse
+  }
 
 }
 
